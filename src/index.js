099a5c7fd3f25441278a41514a6734f686a695de
saisantosh005 <55182141+saisantosh005@users.noreply.github.com>
2022-04-16 15:52:56 +0530

    routingCorrection

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import App from "./App";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes,
-  Switch
-} from "react-router-dom";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
-root.render(
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  rootElement
 );
